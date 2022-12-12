eb706438035b845987ffb4dd8fa46006487a37f1
Valentino Traverso <vt20032003@live.com.ar>
2022-12-12 13:56:00 +0100

    front layer

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const sectionElement = document.getElementById('postBox');
+
+fetch(' http://localhost:3000/posts')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(data => {
+            sectionElement.innerHTML += `<div class="col card p-0">
+            <img src="http://c.files.bbci.co.uk/653B/production/_95151952_mediaitem95151800.jpg" class="card-img-top img-fluid"
+                alt="...">
+            <div class="card-body">
+                <p class="card-title fw-bold">${data.title}</p>
+            </div>
+            </div>`;
+        });
+    })
